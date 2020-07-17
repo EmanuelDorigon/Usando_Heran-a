ef5f1af2e68b6b53b1f67cb2b232ae83d55ec2d3
Emanuel Dorigon <emanueldorigon123@gmail.com>
2020-07-17 10:29:24 -0300

    Project started

---
+++ Entities/BusinessAccount.cs
@@ -0,0 +1,15 @@
+﻿namespace Herança.Entities
+{
+    class BusinessAccount : Account    // A BusinessAccount vai ter tudo o que a Account tiver(Herança)
+    {
+        public double LoanLimit { get; set; }
+        public BusinessAccount() // Construtor padrão, criar o objeto sem passar os dados
+        {
+        }
+
+        public BusinessAccount(int number, string holder, double balance, double loanLimit) : base(number, holder, balance) // Aproveitamos o construtor da classe Account
+        {
+            LoanLimit = loanLimit;
+        }
+    }
+}
